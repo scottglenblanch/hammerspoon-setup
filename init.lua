ab0a0951913d8f91e4ebd3a3bd01c75fb0e76d83
Scott Blanch <scottglenblanch@gmail.com>
2020-09-09 17:44:31 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,18 @@
 local hyper = {"cmd", "alt"}
 
-local mapKeyAndProgram = {
+local mapKeyAndCommand = {
   ["a"] = "atom",
   ["i"] = "intellij",
   ["o"] = "outlook",
+  ["m"] = "browser https://messages.google.com/web/conversations",
   ["s"] = "slack",
   ["t"] = "iterm",
+  ["p"] = "spotify",
+  ["l"] = "lock.machine",
+  ["z"] = "move-photos.sh"
 }
 
-for hotKey, program in pairs(mapKeyAndProgram) do
+for hotKey, program in pairs(mapKeyAndCommand) do
   hs.hotkey.bind(hyper, hotKey, function()
     hs.execute(program, true)
   end)
